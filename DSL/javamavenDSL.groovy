df31ea1ae8d977539c669ff07de4e7027f63e748
Maxi Robledo <maxirobledo@gmail.com>
2022-07-25 18:16:04 -0300

    DSL file modified

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -49,6 +49,6 @@ job('job-DSL-java-maven') {
 job('Test-Hola-Mundo') {
     description('Job DSL test hola mundo para el curso de Jenkins')
     steps {
-      shell('echo Hola Mundo!!!')  
+      shell('echo Hola mundo!!!')  
     }
 }
